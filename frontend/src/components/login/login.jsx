6aecd17cf09aceb60d8e14fcb677ef654dacf330
alishawalunj <69049067+alishawalunj@users.noreply.github.com>
2024-05-28 20:20:33 +0530

    login page fields update

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -4,9 +4,17 @@ const Login = () =>{
         <div className = "login">
             <div className = "loginWrapper">
                 <form action="">
-                    <input type="text" placeholder="username"/>
-                    <input type="password" placeholder='password'/>
+                    <h1>Login Here</h1>
+                    <input type="text" placeholder="username" required/>
+                    <input type="password" placeholder='password' required/>
+                    <div className="remember-forget">
+                        <label><input type="checkbox"/>   Remember me</label>
+                        <a href="forgotPassword.jsx">Forgot Password?</a>
+                    </div>
                     <button>Login</button>
+                    <div className="register">
+                        <p>Don't have an account?<a href="register.jsx">Register here</a></p>
+                    </div>
                 </form>
             </div>
 
