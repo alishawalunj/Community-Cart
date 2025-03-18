915b2c6815b90584b5999015b7d2b7ce37328333
ALISHA WALUNJ <walunjalisha@gmail.com>
2025-03-18 00:11:29 -0400

    removed user_management service

--- server/user_management/src/main/java/com/nzefler/user_management/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nzefler.user_management.repository;
-
-import com.nzefler.user_management.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findUserByUserName(String userNme);
-}
